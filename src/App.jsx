6d18258c2998bdc60e7019c505c9406fc97fee9c
Junaid <06Junaidsheikh>
2026-01-01 09:56:15 +0500

    setup checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <Routes>
         <Route index element = {<HomePage />} />
-        <Route path='checkout' element = {<div>test</div>} />
+        <Route path='checkout' element = {<CheckoutPage />} />
       </Routes>
     </>
   )
