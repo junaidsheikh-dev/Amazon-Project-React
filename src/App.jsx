3facb27ae56fe259781388a69e603500564729b2
Junaid <06Junaidsheikh>
2026-01-13 16:16:44 +0500

    make tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route } from 'react-router'
-import { HomePage } from './pages/homepage/HomePage'
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OdersPage } from './pages/orderspage/OrdersPage'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/homepage/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OdersPage } from "./pages/orderspage/OrdersPage";
+import { TrackingPackage } from "./pages/tracking package/Tracking";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route index element = {<HomePage />} />
-        <Route path='checkout' element = {<CheckoutPage />} />
-        <Route path='orders'  element = {<OdersPage />} />
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OdersPage />} />
+        <Route path="tracking" element={<TrackingPackage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/orderspage/OrdersPage.jsx
+++ src/pages/orderspage/OrdersPage.jsx
@@ -54,7 +54,7 @@ export function OdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <a href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
@@ -80,7 +80,7 @@ export function OdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <a href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
@@ -129,7 +129,7 @@ export function OdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="tracking.html">
+                <a href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
