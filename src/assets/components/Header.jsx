1f7ca142131206ec818c8ac637b2f53e063cf5f1
Junaid <06Junaidsheikh>
2026-01-13 14:46:18 +0500

    split header into components

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,41 @@
+import "./Header.css";
+import { Link } from "react-router";
+import logoWhite from "../images/logo-white.png";
+import searchIcon from "../images/icons/search-icon.png";
+import cartIcon from "../images/icons/cart-icon.png";
+import mobileLogo from "../images/mobile-logo-white.png";
+
+export function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="left-section">
+          <Link to="/" className="header-link">
+            <img className="logo" src={logoWhite} />
+            <img className="mobile-logo" src={mobileLogo} />
+          </Link>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src={searchIcon} />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <Link className="orders-link header-link" to="/orders">
+            <span className="orders-text">Orders</span>
+          </Link>
+
+          <Link className="cart-link header-link" to="/checkout">
+            <img className="cart-icon" src={cartIcon} />
+            <div className="cart-quantity">3</div>
+            <div className="cart-text">Cart</div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,43 +1,13 @@
-import "./header.css";
+import { Header } from "../assets/components/Header";
 import "./OrdersPage.css";
-import logoWhite from "../assets/images/logo-white.png";
-import searchIcon from "../assets/images/icons/search-icon.png";
-import cartIcon from "../assets/images/icons/cart-icon.png";
-import mobileLogo from "../assets/images/mobile-logo-white.png";
 import buyagainIcon from "../assets/images/icons/buy-again.png";
 
 export function OdersPage() {
   return (
     <>
       <title>Orders</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src={logoWhite} />
-            <img className="mobile-logo" src={mobileLogo} />
-          </a>
-        </div>
 
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src={searchIcon} />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src={cartIcon} />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header />
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
